af5007c9b4106f5b723891e8c23ff1102d1497de
arbitrary hexcode <0XDE57@users.noreply.github.com>
2023-08-21 21:22:06 -0600

    second batch wetlands

--- generate_heatmaps.py
+++ generate_heatmaps.py
@@ -1,5 +1,7 @@
 from ptable_trends import ptable_plotter
 
+# row [87-118] are highjacked for custom values (so4, ph, conductivity, etc)
+#
 
 # file = "AOSTRA_KM9_median.csv"
 # file = "BM11_median.csv"
@@ -12,7 +14,17 @@ from ptable_trends import ptable_plotter
 # file = "High_Sulfate_median.csv"
 # file = "Jenny_median.csv"
 # file = "Jetliner_median.csv"
-file = "JP302_median.csv"
+# file = "JP302_median.csv"
+# file = "JP311_median.csv"
+# file = "Linus_median.csv"
+# file = "Maqua_median.csv"
+# file = "NE7_median.csv"
+# file = "Pat_median.csv"
+# file = "Poplar_Marsh_median.csv"
+# file = "Ruth_Lake_median.csv"
+# file = "La_Saline_median.csv"
+# file = "Tower_median.csv"
+file = "WF4_median.csv"
 
 ptable_plotter(filename=file, extended=False, cmap="inferno")
 
